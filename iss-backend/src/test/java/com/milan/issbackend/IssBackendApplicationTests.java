9338765089752b5746c8abb5b35dea259c32edca
Milan Paradina <milanparadina83@gmail.com>
2026-03-18 14:56:28 +0100

    create initial backend project

---
+++ iss-backend/src/test/java/com/milan/issbackend/IssBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milan.issbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

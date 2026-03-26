350e0b36da267d55814aaed6d27b74df115588ec
Milan Paradina <milanparadina83@gmail.com>
2026-03-26 22:21:24 +0100

    add auth configs

---
+++ iis_backend/src/main/java/com/milan/iis_backend/security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.milan.iis_backend.security;
+
+import com.milan.iis_backend.security.jwt.JwtAuthFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .headers(h -> h.frameOptions(f -> f.disable())) // samo za H2 console
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+}

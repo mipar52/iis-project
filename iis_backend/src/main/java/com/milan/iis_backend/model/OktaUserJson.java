74c1a1abeb784dced40397ea7566342c912365f0
Milan Paradina <milanparadina83@gmail.com>
2026-03-26 18:06:10 +0100

    update XML & JSON schemas

--- iis_backend/src/main/java/com/milan/iis_backend/model/OktaUserJson.java
+++
@@ -1,9 +0,0 @@
-package com.milan.iis_backend.model;
-
-public class OktaUserJson {
-    public String firstName;
-    public String lastName;
-    public String email;
-    public String login;
-    public String mobilePhone;
-}

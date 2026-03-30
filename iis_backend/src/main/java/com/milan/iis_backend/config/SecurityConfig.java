159648d5a5ae89be5ee91453220a53b6a89af173
Milan Paradina <milanparadina83@gmail.com>
2026-03-30 11:47:43 +0200

    add http method config for different roles in sec chain

--- iis_backend/src/main/java/com/milan/iis_backend/config/SecurityConfig.java
+++ iis_backend/src/main/java/com/milan/iis_backend/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.milan.iis_backend.config;
 import com.milan.iis_backend.security.jwt.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,9 +19,9 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable())
-                .formLogin(form -> form.disable())
-                .httpBasic(basic -> basic.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable) // maknuo jer bi fe bacao login formu prilikom 401/403, i to mi je bas islo na zive
+                .httpBasic(AbstractHttpConfigurer::disable)
                 .exceptionHandling(ex -> ex
                         .authenticationEntryPoint((request, response, authException) -> {
                             response.setStatus(401);
@@ -34,14 +36,31 @@ public class SecurityConfig {
                 )
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        // everyone can login
                         .requestMatchers("/api/auth/login").permitAll()
+
+                        // everyone can register :D
                         .requestMatchers("/api/auth/register").permitAll()
+
+                        // db access
                         .requestMatchers("/h2-console/**").permitAll()
+
+                        // both users and admin can read data
+                        .requestMatchers(HttpMethod.GET, "/api/**").hasAnyRole("USER", "ADMIN")
+
+                        // only admin can do create/update/delete
+                        .requestMatchers(HttpMethod.POST, "/api/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PUT, "/api/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/api/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/api/**").hasRole("ADMIN")
+
+                        // soap access
                         .requestMatchers("/ws/**").permitAll()
+
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .headers(h -> h.frameOptions(f -> f.disable())) // samo za H2 console
+                .headers(h -> h.frameOptions(f -> f.disable()))
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }

9b556ed50194b9f31c0c06fd5c55a01f0acfd70e
Milan Paradina <milanparadina83@gmail.com>
2026-03-30 10:41:33 +0200

    add secutiry attribute to sec config

--- iis_backend/src/main/java/com/milan/iis_backend/config/SecurityConfig.java
+++ iis_backend/src/main/java/com/milan/iis_backend/config/SecurityConfig.java
@@ -4,11 +4,13 @@ import com.milan.iis_backend.security.jwt.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
 

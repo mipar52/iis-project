ee0ad3522527f9839ea46bb9d0183ef14918df88
Milan Paradina <milanparadina83@gmail.com>
2026-03-30 10:41:54 +0200

    add role checking in graphql controller

--- iis_backend/src/main/java/com/milan/iis_backend/controller/GraphQlController.java
+++ iis_backend/src/main/java/com/milan/iis_backend/controller/GraphQlController.java
@@ -15,6 +15,7 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,27 +30,31 @@ import java.util.UUID;
 public class GraphQlController {
     private final UserRepository userRepository;
     private final UserGateway userGateway;
-
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     @QueryMapping
     public List<OktaUserDto> users() {
         return userGateway.list();
     }
 
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
     @QueryMapping
     public OktaUserDto user(@Argument("id") String id) {
         return userGateway.get(id);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @MutationMapping
     public OktaUserDto createUser(@Argument("input") CreateOktaUserDto createUserInput) {
         return userGateway.create(createUserInput);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @MutationMapping
     public OktaUserDto updateUser(@Argument String id, @Argument("input") UpdateOktaUserDto updateOktaUserDto) {
         return userGateway.update(id, updateOktaUserDto);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @MutationMapping
     public Boolean deleteUser(@Argument("id") String id) {
         userGateway.delete(id);
